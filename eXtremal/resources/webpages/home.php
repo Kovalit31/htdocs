4eaafe5e7efbaa3d7f03adc6bac321d48214c1fb
Kovalit31 <73341678+Kovalit31@users.noreply.github.com>
2022-05-05 15:26:31 +0300

    OK

--- eXtremal/resources/webpages/home.php
+++ eXtremal/resources/webpages/home.php
@@ -6,13 +6,13 @@
     <body>
         <?php
                 if ($_COOKIE["language"]=="ru") {
-                    echo "<h1>Установка прошла успешно!</h1>Если изображается не правильно, то посмотрите<a href=\"./guides/install-apache-eng.html\">здесь</a><br>Не то язык? <a href=\"/index.html\">Поменять</a><br>";
+                    echo "<h1>Установка прошла успешно!</h1>Если изображается не правильно, то посмотрите<a href=\"./guides/install-apache-eng.html\">здесь</a><br>Не то язык? <a href=\"./reset.html\">Поменять</a><br>";
                 }
                 elseif ($_COOKIE["language"] == "lv") {
-                        echo "<h1></h1>Ja izskatas nepareizi, skatities <a href=\"./guides/install-apache-eng.html\"> šeit</a><br> Nepareiza valoda? <a href=\"/index.html\"> Izmainīt </a> <br>";
+                        echo "<h1></h1>Ja izskatas nepareizi, skatities <a href=\"./guides/install-apache-eng.html\"> šeit</a><br> Nepareiza valoda? <a href=\"./reset.html\"> Izmainīt </a> <br>";
                 } 
                 elseif ($_COOKIE["language"] == "en"){
-                    echo "Not your language? <a href=\"/index.html\">Select out!</a><br>";
+                    echo "Not your language? <a href=\"./reset.html\">Select out!</a><br>";
                 }
                 else {
                     echo "Cannot Load SITE";
