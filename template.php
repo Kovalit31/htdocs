6610ec4e888d281230da1bbdaab372a2a04fe513
Kovalit31 <73341678+Kovalit31@users.noreply.github.com>
2022-05-05 15:54:19 +0300

    Good

---
+++ template.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="/eXtremal/resources/style/for_all.css">
+        <?php 
+            if($_COOKIE["theme"] == "dark") {
+                echo "<link rel=\"stylesheet\" href=\"/eXtremal/resources/style/dark.css\">";
+            } elseif($_COOKIE["theme"] == "light") {
+                echo "<link rel=\"stylesheet\" href=\"/eXtremal/resources/style/light.css\">";
+            } else {
+                echo "<script> window.location.href=\"/index.html\" </script>";
+            }
+        ?>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <table id="container-content" border="1">
+            <tr class="hat">
+                <td colspan = "3" id="hat">
+                <?php 
+                    # Hat
+                    if($_COOKIE["language"] == "en"){
+                        echo "";
+                    } elseif($_COOKIE["language"] == "ru") {
+                        echo "";
+                    } elseif($_COOKIE["language"] == "lv") {
+                        echo "";
+                    } else {
+                        echo "Go to <a href=\"/eXtremal/resources/index.php\">setter</a>";
+                    }
+                ?>
+                </td>
+            </tr>
+            <tr class="content">
+                <!--<td id="content-left">p</td>-->
+                <td id="content-center">
+                <?php 
+                    # Content center
+                    if($_COOKIE["language"] == "en"){
+                        echo "";
+                    } elseif($_COOKIE["language"] == "ru") {
+                        echo "";
+                    } elseif($_COOKIE["language"] == "lv") {
+                        echo "";
+                    } else {
+                        echo "Go to <a href=\"/eXtremal/resources/index.php\">setter</a>";
+                    }
+                ?></td>
+                <td id="content-right">
+                <?php 
+                    # Content right
+                    if($_COOKIE["language"] == "en"){
+                        echo "";
+                    } elseif($_COOKIE["language"] == "ru") {
+                        echo "";
+                    } elseif($_COOKIE["language"] == "lv") {
+                        echo "";
+                    } else {
+                        echo "Go to <a href=\"/eXtremal/resources/index.php\">setter</a>";
+                    }
+                ?>
+                </td>
+            </tr>
+            <tr class="footer">
+                <td colspan="3" id="footer">
+                <?php 
+                    # Footer
+                    if($_COOKIE["language"] == "en"){
+                        echo "";
+                    } elseif($_COOKIE["language"] == "ru") {
+                        echo "";
+                    } elseif($_COOKIE["language"] == "lv") {
+                        echo "";
+                    } else {
+                        echo "Go to <a href=\"/eXtremal/resources/index.php\">setter</a>";
+                    }
+                ?>
+                </td>
+            </tr>
+            </table>
+    </body>
+<html>
